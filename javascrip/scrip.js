7f9d9e83f929ad762a2bb50ebcec2740a24fa045
xruteforx9 <126337745+xruteforx9@users.noreply.github.com>
2023-05-18 00:20:00 -0500

    Add files via upload

---
+++ javascrip/scrip.js
@@ -0,0 +1,10 @@
+const typed = new Typed(".multiple-text", {
+    strings: [
+      _0x756cb7(0x1e7),
+      "estudiante\x20de\x20Ingenieria\x20en\x20Sistemas",
+    ],
+    typeSpeed: 0x64,
+    backSpeed: 0x64,
+    backDelay: 0x3e8,
+    loop: !![],
+  })
